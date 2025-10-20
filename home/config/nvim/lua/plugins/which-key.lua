0048a057a38f385a620f40ad35410a567d856d22
Matthew <matthew@blu.shortofcode.net>
2025-10-20 12:37:08 -0400

    some formatting

--- home/config/nvim/lua/plugins/which-key.lua
+++ home/config/nvim/lua/plugins/which-key.lua
@@ -1,18 +1,18 @@
 
 return { -- Show available keybindings as you type
-    "folke/which-key.nvim",
-    event = "VimEnter",
-    opts = {
-	delay = 0,
-	icons = {
-	    mappings = vim.g.have_nerd_font,
-	    keys = {},
-	    -- Document existing key chains
-	    spec = {
-		{ "<leader>s", group = "[S]earch" },
-		{ "<leader>t", group = "[T]oggle" },
-		{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-	    },
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	opts = {
+		delay = 0,
+		icons = {
+			mappings = vim.g.have_nerd_font,
+			keys = {},
+			-- Document existing key chains
+			spec = {
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+			},
+		},
 	},
-    },
 }
